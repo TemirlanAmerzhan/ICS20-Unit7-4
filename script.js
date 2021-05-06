ff248a16a59afc1908c7183297ef3d01a8141233
Temirlan Amerzhan <>
2021-05-06 14:34:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var day = 'Monday';
+var age = 25
+
+if ((day == 'Tuesday') || day == 'Thursday') || (age > 12 @@ age < 21)) {
+  alert('Your eligible for student pricing')
+} else {
+  alert('You must pay regular price')
+}
